49df790cd149c9f761a0e39b0988b270bd3f6a3a
Vitor Vianna <vitormatheusfv@gmail.com>
2023-04-17 21:39:33 -0300

    Database connector created

---
+++ src/db/DB.java
@@ -0,0 +1,51 @@
+package db;
+
+import java.sql.*;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection()  {
+
+        try {
+            if (conn == null) {
+                String url = "jdbc:mysql://localhost:3306/coursejdbc?user=developer&password=1234&useSSL=false";
+                conn = DriverManager.getConnection(url);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
