944038336d9564a6211dc9bf9701cf40657a7d32
Vitor Vianna <vitormatheusfv@gmail.com>
2023-04-17 21:53:02 -0300

    Created department class

---
+++ src/application/Programa.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Programa {
+    public static void main(String[] args) {
+
+        Departamento departamento = new Departamento(1, "Logística");
+        System.out.println(departamento);
+
+    }
+}
